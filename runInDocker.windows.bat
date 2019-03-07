881d3abf9b3a7ec25ea7c9cde3fcc089afec48b7
Dmitriy Finozhenok <dmitriy.finozhenok@studiomobile.ru>
2019-03-07 20:01:52 +0900

    windows tests: fix dir creating command

--- runInDocker.windows.bat
+++ runInDocker.windows.bat
@@ -1,4 +1,4 @@
-New-item out -ItemType directory
+mkdir out
 
 docker build -f Dockerfile.tests.windows -t aspose-words-cloud-cpp-test:windows .
 docker run --rm -m 2g -v "%cd%/scripts:C:/scripts" -v "%cd%/out:C:/out" aspose-words-cloud-cpp-test:windows cmd /c ".\scripts\runAll.bat %1 %2 %3
