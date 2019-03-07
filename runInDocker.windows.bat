5f37871d6ec2a25e88d2b36da3d093802593b3b5
Dmitriy Finozhenok <dmitriy.finozhenok@studiomobile.ru>
2019-03-07 19:51:07 +0900

    .gitlab-ci.yml: Remove tty mode and add report support

--- runInDocker.windows.bat
+++ runInDocker.windows.bat
@@ -1,2 +1,2 @@
 docker build -f Dockerfile.tests.windows -t aspose-words-cloud-cpp-test:windows .
-docker run --rm -m 2g -v "%cd%/scripts:C:/scripts" -v "%cd%/out:C:/out" -i -t aspose-words-cloud-cpp-test:windows cmd /c ".\scripts\runAll.bat %1 %2 %3
+docker run --rm -m 2g -v "%cd%/scripts:C:/scripts" -v "%cd%/out:C:/out" aspose-words-cloud-cpp-test:windows cmd /c ".\scripts\runAll.bat %1 %2 %3
