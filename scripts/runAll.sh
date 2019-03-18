f8e240a704cf89e53c040268d2316408bef57a48
Dmitriy Finozhenok <dmitriy.finozhenok@studiomobile.ru>
2019-03-18 18:29:57 +0900

    Do not recreate build dir

--- scripts/runAll.sh
+++ scripts/runAll.sh
@@ -1,6 +1,24 @@
 git clone https://github.com/Microsoft/cpprestsdk.git
 
-mkdir -p aspose-words-cloud-cpp/out
+# Compile cppcheck
+cd /usr/bin/cppcheck-sources
+make SRCBUILD=build CFGDIR=/usr/bin/cppcheck-sources
+make install CFGDIR=/usr/bin/cppcheck-sources
+cp cppcheck /usr/bin/cppcheck./
+
+# Compile cpprestsdk
+cd /
+mkdir build
+mkdir install
+cmake -DCPPREST_EXCLUDE_WEBSOCKETS=ON -DCPPREST_EXCLUDE_COMPRESSION=ON -DCPPREST_EXCLUDE_BROTLI=ON -DBUILD_TESTS=OFF -DBUILD_SAMPLES=OFF -DCMAKE_INSTALL_PREFIX=install/cpprestsdk -S cpprestsdk -B build
+cmake --build build --config Debug --target install
+cmake --build build --config Release --target install
+
+# Compile aw
+mkdir -p aspose-words-cloud-cpp/build
+
+cmake -Dcpprestsdk_ROOT=install/cpprestsdk -DCMAKE_BUILD_TYPE=Debug -S aspose-words-cloud-cpp -B aspose-words-cloud-cpp/build 
+cmake --build aspose-words-cloud-cpp/build --config Debug --target all_unity -- VERBOSE=1
 
 # Generate credentials
 echo "{
@@ -8,13 +26,6 @@ echo "{
         \"AppSid\" : \"$2\",
         \"BaseUrl\" : \"$3\"
       }" > aspose-words-cloud-cpp/servercreds.json
-
-# Compile aw
-rm -r -f aspose-words-cloud-cpp/build
-mkdir aspose-words-cloud-cpp/build
-
-cmake -Dcpprestsdk_ROOT=install/cpprestsdk -DCMAKE_BUILD_TYPE=Debug -S aspose-words-cloud-cpp -B aspose-words-cloud-cpp/build 
-cmake --build aspose-words-cloud-cpp/build --config Debug --target all_unity -- VERBOSE=1
 
 # Run tests
 cmake -E chdir aspose-words-cloud-cpp/build ctest -V -C Debug
@@ -24,6 +35,8 @@ cppcheck aspose-words-cloud-cpp  --quiet --xml -iboost/ -iinstall/ -ithirdparty/
 --platform=win32W --enable=all --force --std=c++11 --suppress=useInitializationList --suppress=unusedFunction \
 -Iaspose-words-cloud-cpp/sources/ -Iaspose-words-cloud-cpp/sources/model -Iaspose-words-cloud-cpp/sources/model/requests --suppress=missingIncludeSystem --suppress=missingInclude \
 --suppress=unmatchedSuppression --output-file=checkResult.xml
+
+mkdir -p aspose-words-cloud-cpp/out
 
 cp aspose-words-cloud-cpp/build/tests/test_result.xml aspose-words-cloud-cpp/out/test_result.xml
 cp checkResult.xml aspose-words-cloud-cpp/out/checkResult.xml
